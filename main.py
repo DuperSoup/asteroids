0e2830d5278572e02cd464c683ef315aa32c84f4
DuperSoup <dupersoup96@gmail.com>
2024-12-30 01:42:41 -0500

    B: added circleshape, updated constants, added player, and updated main to Ch2 L8.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # source venv/bin/activate
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -16,10 +17,28 @@ print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
+
+updateable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updateable, drawable)
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return
+            break
     screen.fill((0, 0, 0))
+
+    for thing in updateable:
+        thing.update(dt)
+    for thingie in drawable:
+        thingie.draw(screen)
+    # player.update(dt)
+    # player.draw(screen)
+
     pygame.display.flip()
+    # clock.tick(60)
+    dt = clock.tick(60)/1000
